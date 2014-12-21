044741458da3ab3105990eaa81838fe339cab457
Michal Špaček <tupinek@gmail.com>
2014-12-21 14:44:54 +0100

    First version.

---
+++ Tube.pm
@@ -0,0 +1,46 @@
+package Task::Map::Tube;
+
+# Pragmas.
+use strict;
+use warnings;
+
+# Version.
+our $VERSION = 0.01;
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Task::Map::Tube - Install the Map::Tube modules.
+
+=head1 SEE ALSO
+
+L<Map::Tube>,
+
+=head1 REPOSITORY
+
+L<https://github.com/tupinek/Taks-Map-Tube>
+
+=head1 AUTHOR
+
+Michal Špaček L<mailto:skim@cpan.org>
+
+L<http://skim.cz>
+
+=head1 LICENSE AND COPYRIGHT
+
+ © 2014 Michal Špaček
+ Artistic License
+ BSD 2-Clause License
+
+=head1 VERSION
+
+0.01
+
+=cut
